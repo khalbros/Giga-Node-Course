1dbff1921a56e5f5ec6ea5732764412a1631502c
Muhammad Ghali Umar <muhammadghali63@gmail.com>
2022-02-10 22:48:05 +0100

    deconstructing functions/methods from a module

--- learn-module/module-one.js
+++ learn-module/module-one.js
@@ -1,12 +1,23 @@
-const https = require("https")
+// const https = require("https")
 
-const req = https.request("https://mern-todo-tracker.herokuapp.com", (res) => {
-  res.on("data", (chunk) => {
-    console.log("Data recieved: " + chunk)
+// const req = https.request("https://mern-todo-tracker.herokuapp.com", (res) => {
+//   res.on("data", (chunk) => {
+//     console.log("Data recieved: " + chunk)
+//   })
+//   res.on("end", () => {
+//     console.log("No more Data")
+//   })
+// })
+
+// req.end()
+
+const {get} = require("https")
+
+get("https://mern-todo-tracker.herokuapp.com", (res) => {
+  res.on("data", (data) => {
+    console.log(`Data recieved ${data}`)
   })
   res.on("end", () => {
-    console.log("No more Data")
+    console.log("Data fetching Ends")
   })
 })
-
-req.end()
